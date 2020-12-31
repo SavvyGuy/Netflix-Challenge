cd02253dca88ff9a2f9f2f5eefc3396f4146de14
Nikola Ntasi <n.ntasi@student.tudelft.nl>
2020-12-31 12:27:34 +0100

    Reads CSV files

---
+++ Challenge/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# This is a sample Python script.
+users = pd.read_csv("data/users.csv")
+movies = pd.read_csv("data/movies.csv")
+ratings = pd.read_csv("data/ratings.csv")
+predictions = pd.read_csv("data/predictions.csv")
+submission = pd.read_csv("data/submission.csv")
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
