05c98f36c85d2f8dad775f20521769bb3fa39142
Nikola Ntasi <n.ntasi@student.tudelft.nl>
2020-12-31 13:15:46 +0100

    Fixes issue where title colum in movies.csv had 2 values

--- Challenge/main.py
+++ Challenge/main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 # This is a sample Python script.
+
 users = pd.read_csv("data/users.csv")
 movies = pd.read_csv("data/movies.csv")
 ratings = pd.read_csv("data/ratings.csv")
 predictions = pd.read_csv("data/predictions.csv")
 submission = pd.read_csv("data/submission.csv")
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
