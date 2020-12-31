8d592a709fe1f5ec4c472df22fd2790580429fef
Nada Mouman <N.Mouman@student.tudelft.nl>
2020-12-31 23:33:46 +0100

    modified utility matrix

--- Challenge/main.py
+++ Challenge/main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 
-# This is a sample Python script.
-
-users = pd.read_csv("data/users.csv", delimiter=';', dtype={'userID':'int', 'gender':'str', 'age':'int', 'profession':'int'}, names=['userID', 'gender', 'age', 'profession'])
+# process csv files as arrays
+users = np.array(pd.read_csv("data/users.csv", delimiter=';', dtype={'userID':'int', 'gender':'str', 'age':'int', 'profession':'int'}, names=['userID', 'gender', 'age', 'profession']))
 movies = np.array(pd.read_csv("data/movies.csv", delimiter=';', dtype={'movieID':'int', 'year':'int', 'movie':'str'}, names=['movieID', 'year', 'movie']))
 ratings = np.array(pd.read_csv("data/ratings.csv", delimiter=';', dtype={'userID':'int', 'movieID':'int', 'rating':'int'}, names=['userID', 'movieID', 'rating']))
 predictions = pd.read_csv("data/predictions.csv")
@@ -11,9 +10,13 @@ submission = pd.read_csv("data/submission.csv")
 
 
 if __name__ == '__main__':
-    #TO DO - create a collaborative filtering function like in example
-    utility_matrix = np.zeros((len(users), len(movies)))
+    #TO DO - create a collaborative filtering function
 
+    #entries [0, :] and [:, 0] are empty to process data easier
+    #users x movies matrix
+    utility_matrix = np.zeros((len(users)+1, len(movies)+1))
+
+    #populate matrix with ratings
     for i in ratings:
-        utility_matrix[i[0]-1, i[1]-1] = i[2]
+        utility_matrix[i[0], i[1]] = i[2]
 
